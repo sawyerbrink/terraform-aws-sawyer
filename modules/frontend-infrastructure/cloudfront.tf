9cc1f4411452ba8419003d079fe103ee4fee3e87
mrspanishviking <cardenas88karl@gmail.com>
2021-10-10 10:48:22 -0700

    chore: added missing s3 variables to Cloudfront module

--- modules/frontend-infrastructure/cloudfront.tf
+++ modules/frontend-infrastructure/cloudfront.tf
@@ -8,7 +8,9 @@ module "cloudfront" {
   }
 
   bucket-name             = aws_s3_bucket.code-storage.id
+  bucket-arn              = aws_s3_bucket.code-storage.arn
   bucket-dr-name          = aws_s3_bucket.code-storage-DR.id
+  bucket-dr-arn           = aws_s3_bucket.code-storage-DR.arn
   bucket-domain-name      = aws_s3_bucket.code-storage.bucket_domain_name
   bucket-domain-dr-name   = aws_s3_bucket.code-storage-DR.bucket_domain_name
   s3-replication-role-arn = aws_iam_role.s3-replication.arn
@@ -26,9 +28,9 @@ module "cloudfront" {
     "response_page_path" : "/",
     "error_caching_min_ttl" : "300"
   }]
-  cache_items      = ["/static/*"]
-  hosted-zone-id   = local.domain_hosted_zone_id
-  certificate-arn  = data.aws_acm_certificate.ui-cert.arn
+  cache_items     = ["/static/*"]
+  hosted-zone-id  = local.domain_hosted_zone_id
+  certificate-arn = data.aws_acm_certificate.ui-cert.arn
 
   error_document = "index.html"
 
